4d8afca14dde7377707c1e8976f0fc5e1519d58f
Emmanuel <emmanuelegbebo@gmail.com>
2026-02-23 23:56:40 +0100

    created login sidePanel and added event listeners

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom'
 
 export default function HomePage() {
   const [menuArr, setMenuArr] = useState([])
+  const [isLogInOpen, setIsLogInOpen] = useState(false);
 
   async function fetchMenu () {
     try{
@@ -24,6 +25,7 @@ export default function HomePage() {
   }, [])
 
   console.log(menuArr)
+  
 
   return (
     <>
@@ -33,7 +35,7 @@ export default function HomePage() {
         <div></div> <div></div> <div></div>
         
         <nav>
-          <button className='lg:text-[1rem] md:text-[0.875rem] text-[12px] bg-black text-white lg:px-6 md:px-4 px-4 lg:py-3 md:py-2 py-2 rounded-xl font-bold hover:cursor-pointer'>Sign in</button>
+          <button id='logInBtn' className='lg:text-[1rem] md:text-[0.875rem] text-[12px] bg-black text-white lg:px-6 md:px-4 px-4 lg:py-3 md:py-2 py-2 rounded-xl font-bold hover:cursor-pointer' onClick={() => setIsLogInOpen(true)}>Sign in</button>
         </nav>
         <img src="/images/leftImg.png" className="lg:w-[250px] md:w-[150px] w-[120px] absolute left-0 lg:top-20 md:top-24 top-28 z-0 " />
         <img src="/images/rightImg.png" className="lg:w-[250px] md:w-[150px] w-[120px] absolute right-0 lg:top-20 md:top-24 top-28 z-0" />
@@ -86,24 +88,70 @@ export default function HomePage() {
               </div>
             </div>
           </div>
-
-          <article className='w-full overflow-x-auto bg-white flex flex-col items-center hide-scrollbar'>
-            <div className=' text-black font-bold lg:text-[1.5rem] md:text-[1.25rem] text-[1rem] p-5 w-full'>Order our best food options</div>
-            <div id='FoodsGrid' className="grid grid-flow-col grid-rows-2 gap-4 overflow-x-auto p-5 hide-scrollbar bg-white">
-              {
+          <article className='text-left lg:px-[200px] md:px-[100px] px-[50px] bg-white w-full'>
+            <div className='bg-white flex flex-col items-start justify-center gap-4 lg:w-full md:w-full w-full pt-10 relative z-10'>
+              <h1 className='font-bold text-2xl'>Discover our best food options</h1>
+              <div className='grid grid-rows-2 grid-flow-col lg:gap-5 gap-10 w-full overflow-x-auto overflow-hidden no-scrollbar'>
+                {
                 menuArr.map((food) => {
                   return (
-                    <div key={food._id} className=" p-5 text-center text-lg w-40 flex flex-col justify-evenly shadow-lg">
-                      <img src={`./images/${food.image}`} alt={food.name} className="w-full h-32 object-cover mb-4 rounded" />
-                      <h2 className="text-[1rem] font-thin mb-2">{food.name}</h2>
+                    <div key={food._id} className=" text-center text-lg w-[fit-content] flex flex-col justify-center items-center">
+                      <img src={`./images/${food.image}`} alt={food.name} className="lg:min-w-40 min-w-20" />
+                      <h2 className="text-[1rem] font-medium mb-2">{food.name}</h2>
                     </div>
                   )
                 })
               }
-          </div>
+              </div>
+            </div>
+
           </article>
+
+          <article className='text-left lg:px-[200px] md:px-[100px] px-[50px] bg-white w-full -mt-10'>
+              <div className='bg-white flex flex-col items-start justify-center gap-4 lg:w-full md:w-full w-full pt-10 relative z-10'>
+              <h1 className='font-bold text-2xl'>Discover best restaurants on Dineout</h1> 
+              </div>
+          </article>
+          
         </main>
-      
+        
+        {isLogInOpen && (
+          <aside id="logInModel" className="fixed inset-0 bg-black/50 z-40">
+          <div className="bg-white fixed top-0 right-0 bottom-0 z-20 w-[280px] md:w-[40%] lg:w-[32%]">
+            <div className = ' py-12 px-9 relative'>
+              <button id="closeBtn" className="lg:text-xl absolute top-5 font-thin" onClick = {() => setIsLogInOpen(false)}>
+            <i className="fa-solid fa-xmark "></i>
+            </button>
+
+            <header className='flex flex-row justify-between items-center'>
+              <div className=''>
+                <h1 className='lg:text-3xl md:text-2xl mt-2 lg:mt-5 font-medium'>Login</h1>
+              <h3 className='lg:text-sm md:text-[.7rem] text-[.5rem] w-full'>or <a href="" className='text-[#FF5200]'>create an account</a></h3>
+              </div>
+              <div id='imgDiv'>
+                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r" className='w-[55px] md:w-[70px] lg:w-[100px]' />
+              </div>
+              
+            </header>
+
+            <div id='formDiv'>
+              <form action="" encType='' className='flex flex-col items-center justify-center gap-5'>
+                <input type="text" placeholder='Phone number' className='w-full lg:py-4 md:py-3 py-2 rounded-lg border-2 border-gray-300 mt-10 outline-none px-4 lg:text-[1rem] md:text-[0.875rem] text-[0.75rem]' />
+                <button className='bg-[#FF5200] w-full p-2 text-white rounded-lg lg:py-3'>
+                  LOGIN
+                </button>
+                <small>By clicking on Login, I accept the Terms & Conditions & Privacy Policy</small>
+              </form>
+            </div>
+            </div>
+
+          </div>
+              
+        </aside>
+
+        )}
+        
+
       
     </>
   )
